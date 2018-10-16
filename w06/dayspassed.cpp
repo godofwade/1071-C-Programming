30440d9fc2356e63932c066fe58a3fd145f5aa9f
godofwade <43335888+godofwade@users.noreply.github.com>
2018-10-16 17:51:08 +0800

    Update dayspassed.cpp

--- w06/dayspassed.cpp
+++ w06/dayspassed.cpp
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<ctype.h>
-#include<conio.h>
 
 
-int leap(int ye)
+
+int leap(int y)
 {
-	if(ye%400==0 || (ye%4==0 && ye%100!=0))
+	if(y%400==0 || (y%4==0 && y%100!=0))
 	return 1;
 	else
 	return 0;
@@ -14,29 +14,36 @@ int leap(int ye)
 
 int main()
 {
-	int m,d,ds,ye;
+	int m,d,ds,y;
 	ds=0;
-	char cont='y';
+	char cont= 'Y';
 	
 	int a[12]={31,28,31,30,31,30,31,31,30,31,30,31};
 	
-	while(cont=='y')
+	while(cont=='Y')
 	{
-
+		ds=0;
 		printf("Enter year:");
-		scanf("%d",&ye);
+		scanf("%d",&y);
 		printf("Enter month and day:");
 		scanf("%d %d",&m,&d);
 		a[1]=28;
-		if(leap(ye))
-		a[1]++;
+		if(leap(y))
+		{
+			a[1]++;	
+		}
+		
 		
 		for(int i=0;i<m-1;i++)
-		ds+=a[i];
+		{
+			ds+=a[i];
+		}
 		
-		printf("%d days passed since 1/1/%d\n",ds+d,ye);
+		
+		printf("%d days passed since 1/1/%d\n",ds+d,y);
 		printf("Continue (Y/N)? ");	
-		cont=toupper(getche());
+		getchar();
+		cont=toupper(getchar());
 
 		
 
