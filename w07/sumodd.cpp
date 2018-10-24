a30e4545cb0744715593b72c03866807886c8daf
godofwade <43335888+godofwade@users.noreply.github.com>
2018-10-24 14:53:30 +0800

    Create sumodd.cpp

---
+++ w07/sumodd.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+
+int sumodd1(int n)
+{
+
+	if(n==1)
+	return 1;
+	else
+	return sumodd1(n-1)+(2*n-1);
+}
+
+int sumodd2(int n)
+{
+	if(n==1)
+	return 1;
+	else
+	return sumodd2(n-2)+n;
+}
+
+
+int main()
+{
+	int n;
+	
+	printf("Enter n: ");
+	scanf("%d",&n);
+	while(n>0)
+	{
+
+		printf("sumodd1(%d) = %d\n",n,sumodd1(n));
+		printf("sumodd2(%d) = %d\n",n,sumodd2(n));
+		printf("\n");
+		
+		printf("Enter n: ");
+		scanf("%d",&n);
+	}	
+
+	return 0;
+	
+}
